2c3a14cfac20f25346bd53551869949a7d7d610c
KucharekB <166820658+KucharB@users.noreply.github.com>
2024-11-25 11:18:10 +0100

    Update MUL_UNIT.sv

--- MUL_UNIT.sv
+++ MUL_UNIT.sv
@@ -21,18 +21,12 @@
 
 
 module MUL_UNIT(
-input [15:0] a_val,
-input [15:0] b_val,
-output logic [31:0] result
-    );
-    
-    logic [31:0] interial_a;
-    logic [31:0] interial_b;
-    
-    always_comb begin
-      interial_a = {{16{a_val[15]}},a_val};
-      interial_b = {{16{b_val[15]}},b_val};
+    input [15:0] a_val,
+    input [15:0] b_val,
+    output logic [31:0] result
+);
 
-    result = interial_a * interial_b;
+    always_comb begin
+        result = a_val * b_val;
     end
 endmodule
